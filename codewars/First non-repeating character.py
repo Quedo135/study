f86d0a20b21298bd42d1cb93c8549877fe2f0b14
DmitryAlekhin <alekhin.dm@gmail.com>
2023-03-12 13:06:25 +0200

    first

--- codewars/First non-repeating character.py
+++ codewars/First non-repeating character.py
@@ -1,2 +1,8 @@
 def first_non_repeating_letter(string):
-    #your code here
+    for m in string:
+        if string.lower().count(m.lower()) == 1:
+            return m
+        
+a = 'sTress'
+
+print(first_non_repeating_letter(a))
